69da67947456b66a32dc0de0903a52ad72e36939
LittleHappyDiary <1405379189@qq.com>
2026-07-14 15:53:21 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-#!/usr/bin/env node
-async function start() {
-  // 如果需要手动修改anonymous_token，需要注释generateConfig调用
-  require('./server').serveNcmApi({
-    checkVersion: true,
-  })
-}
-start()
+const { consturctServer } = require('./server')
+
+module.exports = consturctServer()
